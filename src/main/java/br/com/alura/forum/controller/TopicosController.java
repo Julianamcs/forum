b50c90bc9e824d45cef469e343455bb7ff75b442
Juliana Maciel <julyanamcs@gmail.com>
2023-07-13 04:50:18 -0300

    criação da lista e do Cadastro e a resposta de retorno ao cliente
    Que para evitar repetir a URL em todos os métodos, devemos utilizar a anotação @RequestMapping em cima da classe controller;
    Que para mapear requisições do tipo POST, devemos utilizar a anotação @PostMapping;
    Que para receber dados enviados no corpo da requisição, a boa prática é criar uma classe que também siga o padrão DTO (Data Transfer Object);
    Que a boa prática para métodos que cadastram informações é devolver o código HTTP 201, ao invés do código 200;
    Que para montar uma resposta a ser devolvida ao cliente da API, devemos utilizar a classe ResponseEntity do Spring;
    Que para testar requisições do tipo POST, precisamos utilizar alguma ferramenta de testes de API Rest;
    Como utilizar o Postman para testar uma API Rest;

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,22 +1,57 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.DTO.TopicoDto;
-import br.com.alura.forum.model.Curso;
+import br.com.alura.forum.form.TopicoForm;
 import br.com.alura.forum.model.Topico;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
+    @Autowired
+    TopicoRepository topicoRepository;
 
+    @Autowired
+    CursoRepository cursoRepository;
 
+    /**
     @RequestMapping("/topicos")
     public List<TopicoDto> Lista(){
-        Topico topico = new Topico("Dúvidas", "Dúvidas com Spring", new Curso("Spring", "Programaçao"));
+        List<Topico> topicos = topicoRepository.findAll();
+        System.out.println(topicos);
+        return TopicoDto.converter(topicos);
+    }**/
 
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+
+     @GetMapping
+     public List<TopicoDto> Lista(String nomeCurso){
+
+         if (nomeCurso == null) {
+             List<Topico> topicos = topicoRepository.findAll();
+             System.out.println(topicos);
+             return TopicoDto.converter(topicos);
+         }else{
+             List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+             System.out.println(nomeCurso);
+             return TopicoDto.converter(topicos);
+         }
+     }
+
+    @PostMapping
+     public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriComponentsBuilder){
+         Topico topico = form.converter(cursoRepository);
+         topicoRepository.save(topico);
+         URI uri = uriComponentsBuilder.path("/topicos/id").buildAndExpand(topico.getId()).toUri();
+         return ResponseEntity.created(uri).body(new TopicoDto(topico));
+
     }
+
 }
--- src/main/java/br/com/alura/forum/model/Curso.java
+++ src/main/java/br/com/alura/forum/model/Curso.java
@@ -1,17 +1,19 @@
 package br.com.alura.forum.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Curso {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String categoria;
-
-    public Curso(String nome, String categoria) {
-        this.nome = nome;
-        this.categoria = categoria;
-    }
 
 
     @Override
